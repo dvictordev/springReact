43d01e226f0121bcd202aab10d8c579e73dd004f
Victor Duarte <victor.d.dev@gmail.com>
2022-09-06 16:38:58 -0300

    added table

--- frontend/src/components/Container/index.tsx
+++ frontend/src/components/Container/index.tsx
@@ -92,6 +92,36 @@ export function Container() {
                     </div>
                   </td>
                 </tr>
+                <tr>
+                  <td className="show992">#341</td>
+                  <td className="show576">08/07/2022</td>
+                  <td>Anakin</td>
+                  <td className="show992">15</td>
+                  <td className="show992">11</td>
+                  <td>R$ 55300.00</td>
+                  <td>
+                    <div className="dsmeta-red-btn-container">
+                      <div className="dsmeta-red-btn">
+                        <img src={vector} alt="Notificar" />
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="show992">#341</td>
+                  <td className="show576">08/07/2022</td>
+                  <td>Anakin</td>
+                  <td className="show992">15</td>
+                  <td className="show992">11</td>
+                  <td>R$ 55300.00</td>
+                  <td>
+                    <div className="dsmeta-red-btn-container">
+                      <div className="dsmeta-red-btn">
+                        <img src={vector} alt="Notificar" />
+                      </div>
+                    </div>
+                  </td>
+                </tr>
               </tbody>
             </table>
           </div>
